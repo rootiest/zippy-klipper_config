27cb6fc34d3ffd94567b94c47cb9edbbd6808559
Chris <chris@rootiest.com>
2022-09-07 19:33:36 -0400

    oopsie

--- shaper/adxl_shape_y.sh
+++ shaper/adxl_shape_y.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/klipper_config/shaper/shaper_calibrate_x.png
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/klipper_config/shaper/shaper_calibrate_y.png
