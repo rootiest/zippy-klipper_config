beebd273bc8863ef71d2ecd2d66b80994fe0f2b3
Chris <chris@rootiest.com>
2022-12-28 05:04:44 -0500

    add datetime script

---
+++ scripts/datetime.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright (C) 2022 Chris Laprade
+# 
+# This file is part of zippy_config.
+# 
+# zippy_config is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# zippy_config is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with zippy_config.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+# Get the current date and time in a human-readable format
+time=$(date "+%c")
+
+# Return the time
+echo $time
