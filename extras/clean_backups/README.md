06b2c19f18a6986258c5da131f11fe2933c275ac
Chris <chris@rootiest.com>
2023-02-15 16:01:16 -0500

    CLEAN BACKUP FILES

---
+++ extras/clean_backups/README.md
@@ -0,0 +1,11 @@
+# Clean Backup Files
+
+This script will clean all those `printer-####.cfg` files that clutter up the main config directory by moving them to an archive directory during klipper startup.
+
+## Required Files
+
+[clean_backups.cfg](clean_backups.cfg)
+
+[move_files.sh](move_files.sh)
+
+[clean_backups.sh](clean_backups.sh)
---
+++ extras/clean_backups/clean_backups.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (C) 2023 Chris Laprade (chris@rootiest.com)
+# 
+# This file is part of config.
+# 
+# config is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# config is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with config.  If not, see <http://www.gnu.org/licenses/>.
+
+/home/pi/printer_data/config/scripts/move_files.sh /home/pi/printer_data/config/ /home/pi/printer_data/config/archive/ "printer-*.cfg"
