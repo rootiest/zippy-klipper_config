bd38b0dcccf684940508149cc973573ac380670b
Chris <chris@rootiest.com>
2023-05-28 02:14:31 -0400

    fix license

--- scripts/mcu_timing.sh
+++ scripts/mcu_timing.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Copyright (C) 2023 Chris Laprade (chris@rootiest.com)
 # 
-# This file is part of Hephaestus.
+# This file is part of zippy_config.
 # 
-# Hephaestus is free software: you can redistribute it and/or modify
+# zippy_config is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# Hephaestus is distributed in the hope that it will be useful,
+# zippy_config is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with Hephaestus.  If not, see <http://www.gnu.org/licenses/>.
+# along with zippy_config.  If not, see <http://www.gnu.org/licenses/>.
 
 #################################################################################
 #                                                                               #
